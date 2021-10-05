7b5c710fe37cfb9987ce40bcb3e5727f647118cd
Brian McPhillips <B.McPhillips89@gmail.com>
2021-10-05 16:38:41 -0700

    Loading Component

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Loading.css';
+
+const Loading = () => (
+  <img 
+    className={styles.Loading}
+    src="https://media0.giphy.com/media/VJCSpN9VaG7K4p3CUj/source.gif" 
+    alt="Loading"/>
+);
+
+export default Loading;
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import Loading from '../Loading/Loading';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <Loading />;
 }
